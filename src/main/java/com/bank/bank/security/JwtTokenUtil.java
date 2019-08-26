414c5d88e9c9509ff1fe5b99feb514bb7e51ad9c
vamsik140 <vamsik140@gmail.com>
2019-08-26 10:06:02 +0530

    updated to 5 days token

--- src/main/java/com/bank/bank/security/JwtTokenUtil.java
+++ src/main/java/com/bank/bank/security/JwtTokenUtil.java
@@ -17,7 +17,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Component
 public class JwtTokenUtil implements Serializable {
 	private static final long serialVersionUID = -2550185165626007488L;
-	public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+	public static final long JWT_TOKEN_VALIDITY = 120 * 60 * 60 ; //5Days
 	@Value("${jwt.secret}")
 	private String secret;
 
