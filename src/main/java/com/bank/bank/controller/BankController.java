9ddb561241f4d2cde78aca7971bfe61e0f52ba7f
vamsik140 <vamsik140@gmail.com>
2019-08-26 10:04:22 +0530

    updated

--- src/main/java/com/bank/bank/controller/BankController.java
+++ src/main/java/com/bank/bank/controller/BankController.java
@@ -5,57 +5,63 @@ import javax.validation.Valid;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bank.bank.pojo.BranchDetailsReq;
 import com.bank.bank.pojo.CommonPojo;
 import com.bank.bank.responsePojo.AbstractResponse;
 import com.bank.bank.service.BankingService;
-import com.bank.bank.service.BankingServiceImp;
 
 @RestController
 public class BankController {
 
 	private static final Logger LOGGER = Logger.getLogger(BankController.class);
-	
+
 	@Autowired
 	BankingService bankingService;
-	
-	
-    @RequestMapping("/greeting")
-    public String greeting() {
-        return "Hello Greeting";
-    }
 
-    @RequestMapping(value = "/getBankDetails/{ifscCode}", method = RequestMethod.GET)
-	public ResponseEntity<? extends AbstractResponse> getBankDetails(@PathVariable("ifscCode") String ifscCode) throws Exception {
+	@RequestMapping("/greeting")
+	public String greeting() {
+		return "Welcome!!!";
+	}
 
-    	LOGGER.info("getBankDetails--> ifscCode: " + ifscCode);
-    	
-    	System.out.println("getBankDetails");
+	@RequestMapping(value = "/getBankDetails", method = RequestMethod.GET)
+	public ResponseEntity<? extends AbstractResponse> getBankDetails(@RequestParam(name = "ifscCode") String ifscCode)
+			throws Exception {
+
+		LOGGER.info("getBankDetails--> ifscCode: " + ifscCode);
+
+		System.out.println("getBankDetails");
 		return bankingService.getBankDetails(ifscCode);
 	}// end of getBankDetails
-    
-    @RequestMapping(value = "/getAllBankDetails", method = RequestMethod.POST)
-   	public ResponseEntity<? extends AbstractResponse> getAllBankDetails(@Valid @RequestBody CommonPojo pagination) throws Exception {
 
-    	LOGGER.info("getAllBankDetails-->");
-    	
-       	System.out.println("getAllBankDetails");
-   		return bankingService.getAllBankDetails(pagination);
-   	}// end of getAllBankDetails
-    
-    @RequestMapping(value = "/getBranchDetails", method = RequestMethod.POST)
-   	public ResponseEntity<? extends AbstractResponse> getBranchDetails(@Valid @RequestBody BranchDetailsReq req) throws Exception {
+	@RequestMapping(value = "/getAllBankDetails", method = RequestMethod.POST)
+	public ResponseEntity<? extends AbstractResponse> getAllBankDetails(@Valid @RequestBody CommonPojo pagination)
+			throws Exception {
 
-    	LOGGER.info("getBranchDetails-->");
-    	
-       	System.out.println("getBranchDetails");
-   		return bankingService.getAllBankDetails(req);
-   	}// end of getBranchDetails
-    
+		LOGGER.info("getAllBankDetails-->");
+
+		System.out.println("getAllBankDetails");
+		return bankingService.getAllBankDetails(pagination);
+	}// end of getAllBankDetails
+
+	@RequestMapping(value = "/getBranchDetails", method = RequestMethod.POST)
+	public ResponseEntity<? extends AbstractResponse> getBranchDetails(@RequestParam(name = "bankName") String bankName,
+			@RequestParam(name = "city") String city, @Valid @RequestBody CommonPojo pagination)
+			throws Exception {
+
+		LOGGER.info("getBranchDetails-->");
+
+		BranchDetailsReq req = new BranchDetailsReq();
+		req.setBankName(bankName);
+		req.setCity(city);
+		
+		System.out.println("getBranchDetails");
+		return bankingService.getAllBankDetails(req, pagination);
+	}// end of getBranchDetails
+
 }
